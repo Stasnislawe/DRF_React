d3fde1e175c6982329b5364d37607fc4f5fb5614
stas_ <dromanqs@mail.ru>
2025-01-09 23:23:37 +0500

    В Installed_Apps подключение drf_yasg, в TEMPLATES > DIRS указал путь к шаблонам (templates) нужно для отображения swaggerui

--- project/project/settings.py
+++ project/project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api_app',
+    'drf_yasg',
     'corsheaders',
     'rest_framework',
+    'api_app',
 ]
 
 MIDDLEWARE = [
@@ -66,7 +67,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
