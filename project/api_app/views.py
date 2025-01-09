ca7b1bf324b8dc9603e2b52450a6c33dec949017
stas_ <dromanqs@mail.ru>
2025-01-09 15:52:06 +0500

    Изменение вьюхи, добавление таких функций как: get, create, update

--- project/api_app/views.py
+++ project/api_app/views.py
@@ -1,9 +1,65 @@
 from .models import Book
-from serializer import BookSerializer
-from rest_framework import viewsets
+from .serializer import BookSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 
+# Вью для DRF --> вывод всех объектов модели Book
 class BookSerializerView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        if self.action == 'create':
+            serializer = BookSerializer(data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        'status': status.HTTP_200_OK,
+                        'message': 'Успех!',
+                        'id': serializer.instance.pk,
+                    }
+                )
+
+            if status.HTTP_400_BAD_REQUEST:
+                return Response(
+                    {
+                        'status': status.HTTP_400_BAD_REQUEST,
+                        'message': 'Некорректный запрос',
+                        'id': None,
+                    }
+                )
+
+            if status.HTTP_500_INTERNAL_SERVER_ERROR:
+                return Response(
+                    {
+                        'status': status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        'message': 'Ошибка при выполнении операции',
+                        'id': None,
+                    }
+                )
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        book = self.get_object()
+        serializer = BookSerializer(book, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    'state': '1',
+                    'message': 'Изменения в записи внесены'
+                }
+            )
+        else:
+            return Response(
+                {
+                    'state': '0',
+                    'message': serializer.errors
+                }
+            )
