82ea678b5c80f7f5d9f3796e72236dacad88f781
stas_ <dromanqs@mail.ru>
2025-01-14 22:55:16 +0500

    Model BookPart + Author

--- project/api_app/models.py
+++ project/api_app/models.py
@@ -12,3 +12,14 @@ class Book(models.Model):
     cover = models.ImageField(upload_to='cover/%Y/%m/%d/', verbose_name='Обложка книги')
     author = models.CharField(max_length=100, verbose_name='Автор')
 
+
+class BookPart(models.Model):
+    title_part = models.CharField(max_length=120, verbose_name='Название части')
+    text_part = models.TextField(verbose_name='Содержание')
+    part_number = models.IntegerField(verbose_name='Номер части')
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=60, verbose_name='Имя')
+    bio = models.TextField(verbose_name='Биография')
+    photo = models.ImageField(upload_to='authors/%Y/%m/%d/', verbose_name='Фото')
