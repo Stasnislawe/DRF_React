e01c4c8d165f2d9cf4f63410fca41cea6cbeacfb
stas_ <dromanqs@mail.ru>
2025-01-14 22:55:40 +0500

    Serializer BookPart + Author

--- project/api_app/serializer.py
+++ project/api_app/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, BookPart, Author
 
 
 # сериалайзер модели Book со всеми полями
@@ -8,3 +8,15 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = ['id', 'title', 'added_at', 'time_to_read',
                   'text', 'cover', 'author']
+
+
+class BookPartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookPart
+        fields = ['id', 'title_part', 'text_part', 'part_number']
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'bio', 'photo']
