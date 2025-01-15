7853a9d972f69fd016932d78e2c62f39b3e609bb
stas_ <dromanqs@mail.ru>
2025-01-15 21:58:52 +0500

    Новые юрл /book/:id/books - списки частей книги /book/:id/books/:partId - конкретная часть книги /book/:id/authors/ - авторы книги

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { BookList } from './pages/BookList';
 import { BookDetail } from './pages/BookDetail';
+import { BookSeries } from './pages/BookSeries';
+import { BookPartDetail } from './pages/BookPartDetail';
+import { BookAuthors } from './pages/BookAuthors';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<BookList />} />
         <Route path="/book/:id" element={<BookDetail />} />
+        <Route path="/book/:id/books" element={<BookSeries />} />
+        <Route path="/book/:id/books/:partId" element={<BookPartDetail />} />
+        <Route path="/book/:id/authors/" element={<BookAuthors />} />
       </Routes>
     </BrowserRouter>
   );
