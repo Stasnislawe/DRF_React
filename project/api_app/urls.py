f5ff5bf576fbfac381f8d03d3720169cf3fa4d04
stas_ <dromanqs@mail.ru>
2025-01-15 22:03:51 +0500

    +юрл для подключения через апи

--- project/api_app/urls.py
+++ project/api_app/urls.py
@@ -1,11 +1,30 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import BookSerializerView
+from .views import BookSerializerView, BookPartSerializerView, AuthorSerializerView
 
 router = routers.DefaultRouter()
 router.register(r'book', BookSerializerView)
 
 
+book_part = BookPartSerializerView.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+
+
+author_book = AuthorSerializerView.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+
+
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('book/<int:book_author>/authors', author_book),
+    path('book/<int:part_number>/books', book_part),
+    path('book/<int:part_number>/books/<int:pk>', BookPartSerializerView.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy',
+    })),
 ]
