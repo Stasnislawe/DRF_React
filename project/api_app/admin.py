b03f27a36634a49df3eb5ba3ca20096a34c7c2f7
stas_ <dromanqs@mail.ru>
2025-01-15 21:57:41 +0500

    Admin.py для проверки

--- project/api_app/admin.py
+++ project/api_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book, BookPart, Author
 
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(BookPart)
+admin.site.register(Author)
