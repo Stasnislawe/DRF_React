ff683b2a0a68f25dc208340d80d218cb01e67784
stas_ <dromanqs@mail.ru>
2025-01-15 22:02:50 +0500

    Изменение моделей (добавление модели автор и часть книги)

--- project/api_app/models.py
+++ project/api_app/models.py
@@ -3,23 +3,26 @@ from django.db import models
 # Create your models here.
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=60, verbose_name='Имя')
+    bio = models.TextField(verbose_name='Биография')
+    photo = models.ImageField(upload_to='authors/%Y/%m/%d/', verbose_name='Фото')
+
+
 # Модель Книги
 class Book(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название')
     added_at = models.DateTimeField(auto_now_add=True, verbose_name='Время опубликования')
     time_to_read = models.CharField(max_length=50, verbose_name='Среднее время прочтения')
-    text = models.TextField(verbose_name='Содержание')
+    text = models.CharField(verbose_name='Краткое описание чтобы заинтересовать читателя', max_length=120)
     cover = models.ImageField(upload_to='cover/%Y/%m/%d/', verbose_name='Обложка книги')
-    author = models.CharField(max_length=100, verbose_name='Автор')
+    author = models.ManyToManyField(Author, related_name='book_author', verbose_name='Автор книги')
 
 
 class BookPart(models.Model):
     title_part = models.CharField(max_length=120, verbose_name='Название части')
     text_part = models.TextField(verbose_name='Содержание')
-    part_number = models.IntegerField(verbose_name='Номер части')
+    part_number = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name='Часть книги')
+    page_count = models.IntegerField(verbose_name='Количество страниц')
+    part_id = models.IntegerField(verbose_name='Номер части', blank=True)
 
-
-class Author(models.Model):
-    name = models.CharField(max_length=60, verbose_name='Имя')
-    bio = models.TextField(verbose_name='Биография')
-    photo = models.ImageField(upload_to='authors/%Y/%m/%d/', verbose_name='Фото')
