bfc38755d3a89480a1c69a752cd4083a574f448a
stas_ <dromanqs@mail.ru>
2025-01-19 23:01:18 +0500

    Новая модель пользователя

--- project/api_app/models.py
+++ project/api_app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
 
 
 class Author(models.Model):
