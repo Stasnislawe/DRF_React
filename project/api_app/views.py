93cebc34aa294deeaeb1da7cfdd02bc162e1008b
stas_ <dromanqs@mail.ru>
2025-01-19 22:59:20 +0500

    Вью выхода из акка/ вью регистрации

--- project/api_app/views.py
+++ project/api_app/views.py
@@ -1,7 +1,32 @@
+from rest_framework.permissions import IsAuthenticated
 from .models import Book, BookPart, Author
-from .serializer import BookSerializer, BookPartSerializer, AuthorSerializer
+from .serializer import BookSerializer, BookPartSerializer, AuthorSerializer, UserSerializer
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class Logout(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh_token']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 
 class AuthorSerializerView(viewsets.ModelViewSet):
