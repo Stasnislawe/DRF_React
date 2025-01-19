7e245f10c6f4ad3e80e5d97e2ab62b2ba83afe16
stas_ <dromanqs@mail.ru>
2025-01-19 23:02:23 +0500

    Изменение конфига

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,3 @@
 export const USE_MOCK_DATA = false; // Toggle between mock (true) and API (false)
 export const API_BASE_URL = 'http://127.0.0.1:8000';
+export const RANDOM_AUTHORS_LIMIT = 1; // Number of random authors to display
