2f01fbe385729cc0c0a72e299796c96de357b5d2
stas_ <dromanqs@mail.ru>
2025-08-28 13:55:43 +0500

    Backend - Комментирование юрл, убрал лишние строки

--- project/api_app/urls.py
+++ project/api_app/urls.py
@@ -1,9 +1,5 @@
 from django.urls import include, path, re_path
-from rest_framework import routers
 from .views import BookSerializerView, BookPartSerializerView, AuthorSerializerView, Logout, RegisterView, BookFreeDetailSerializerView
-
-# router = routers.DefaultRouter()
-# router.register(r'book/(?P<free>True|False)', BookFreeDetailSerializerView)
 
 
 book_part = BookPartSerializerView.as_view({
@@ -19,31 +15,23 @@ author_book = AuthorSerializerView.as_view({
 
 
 urlpatterns = [
-    # path('', include(router.urls)),
+    # Список бесплатных книг
     path('book/<str:free>/', BookSerializerView.as_view(), name='book-free'),
+    # Список частей бесплатной книги
     path('book/<str:free>/<int:pk>/', BookFreeDetailSerializerView.as_view({
         'get': 'retrieve',
     })),
-    path('book/<int:part_number>/books', book_part),
 
+    # Список частей в книге (нужна авторизация)
+    path('book/<int:part_number>/books', book_part),
     path('book/', BookSerializerView.as_view(), name='book-list'),
-    # path('book/<str:free>/<int:pk>/', BookFreeDetailSerializerView.as_view({
-    #     'get': 'retrieve',
-    #     'put': 'update',
-    #     'delete': 'destroy',
-    # })),
-    # re_path('book', BookSerializerView.as_view()),
-    # re_path('book/(?P<book_free>.+)/', BookSerializerView.as_view()),
-    # re_path('book/<int:pk>/(?P<book_free>.+)/', BookFreeDetailSerializerView.as_view({
-    #     'get': 'retrieve',
-    #     'put': 'update',
-    #     'delete': 'destroy',
-    # })),
-    # path('', include(router.urls)),
+    # выход
     path('logout/', Logout.as_view(), name='logout'),
+    # регистрация
     path('register/', RegisterView.as_view(), name='register'),
+    # автор книги
     path('book/<int:book_author>/authors', author_book),
-    # path('book/<int:part_number>/books', book_part),
+    # просмотр определнной части книги
     path('book/<int:part_number>/books/<int:part_id>', BookPartSerializerView.as_view({
         'get': 'retrieve',
         'put': 'update',
