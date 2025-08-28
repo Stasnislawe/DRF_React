9d5df6b276a22f9a3ded06b0f22cb547111ea5eb
stas_ <dromanqs@mail.ru>
2025-08-28 14:00:36 +0500

    Backend - Комментирование сериалайзеров

--- project/api_app/serializer.py
+++ project/api_app/serializer.py
@@ -3,6 +3,7 @@ from .models import Book, BookPart, Author, User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """Сериайлайзер модели User"""
     class Meta:
         model = User
         fields = ['id', 'username', 'password']
@@ -19,8 +20,8 @@ class UserSerializer(serializers.ModelSerializer):
         return instance
 
 
-# сериалайзер модели Book со всеми полями
 class BookSerializer(serializers.ModelSerializer):
+    """Сериалайзер модели Book"""
     class Meta:
         model = Book
         fields = ['id', 'title', 'added_at', 'time_to_read',
@@ -28,12 +29,14 @@ class BookSerializer(serializers.ModelSerializer):
 
 
 class BookPartSerializer(serializers.ModelSerializer):
+    """Сериалайзер модели BookPart"""
     class Meta:
         model = BookPart
         fields = ['id', 'title_part', 'image_part', 'text_part', 'part_number', 'page_count', 'part_id']
 
 
 class AuthorSerializer(serializers.ModelSerializer):
+    """Сериалайзер модели Author"""
     class Meta:
         model = Author
         fields = ['id', 'name', 'bio', 'photo']
